6796e8c51cf900a24681503936fb72766d5664ed
Alexis Moody <amoodyldes@gmail.com>
2018-09-08 10:27:06 -0400

    Sets up basic react app structure

---
+++ app/controllers/referees_controller.rb
@@ -0,0 +1,10 @@
+class RefereesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
---
+++ app/javascript/packs/MainApp/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HomePage extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Welcome to Refs-R-Us</h1>
+      </div>
+    )
+  }
+}
+
+export default HomePage
