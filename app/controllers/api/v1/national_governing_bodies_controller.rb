7b4ccd276338707958a3e43c475c89071f51544b
Alexis Moody <amoodyldes@gmail.com>
2018-09-22 18:05:28 -0400

    Adds NGB controller, serializer, and spec

---
+++ app/controllers/api/v1/national_governing_bodies_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class NationalGoverningBodiesController < ::ApplicationController
+      before_action :find_ngb, only: :show
+
+      def index
+        @national_governing_bodies = NationalGoverningBody.all
+
+        json_string = NationalGoverningBodySerializer.new(@national_governing_bodies).serialized_json
+
+        render json: json_string, status: :ok
+      end
+
+      def show
+        json_string = NationalGoverningBodySerializer.new(@ngb).serialized_json
+
+        render json: json_string, status: :ok
+      end
+
+      private
+
+      def find_ngb
+        @ngb = NationalGoverningBody.find_by(id: params[:id])
+      end
+    end
+  end
+end
