7c5b2b34af8bd38ce55ea016c836b63c9e1f980f
Alexis Moody <amoodyldes@gmail.com>
2018-09-22 16:00:33 -0400

    Adds update action to referee controller

--- app/controllers/api/v1/referees_controller.rb
+++ app/controllers/api/v1/referees_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
-    class RefereesController < ApplicationController
+    class RefereesController < ::ApplicationController
+      before_action :authenticate_referee!, only: :update
       before_action :find_referee, only: %i[show update]
 
       layout false
@@ -10,22 +11,45 @@ module Api
 
         json_string = RefereeSerializer.new(@referees).serialized_json
 
-        render json: json_string
+        render json: json_string, status: :ok
       end
 
       def show
         json_string = RefereeSerializer.new(@referee, {include: [:national_governing_bodies, :certifications]}).serialized_json
 
-        render json: json_string
+        render json: json_string, status: :ok
       end
 
       def update
+        national_governing_body_ids = params.delete(:national_governing_body_ids)
+        update_national_governing_bodies(national_governing_body_ids) if national_governing_body_ids.present?
+
+        if @referee.update!(permitted_params)
+          json_string = RefereeSerializer.new(@referee).serialized_json
+
+          render json: json_string, status: :ok
+        else
+          render json: { error: @referee.errors.message }, status: :unprocessable_entity
+        end
       end
 
       private
 
+      def update_national_governing_bodies(ngb_ids)
+        current_ngb_ids = @referee.national_governing_bodies.pluck(:id)
+        national_governing_body_remove = current_ngb_ids - ngb_ids
+        national_governing_body_add = ngb_ids - current_ngb_ids
+        return unless national_governing_body_remove.present? || national_governing_body_add.present?
+
+        to_remove = @referee.referee_locations.where(national_governing_body_id: national_governing_body_remove)
+        to_remove.destroy_all if to_remove.present?
+
+        ngb_records = NationalGoverningBody.where(id: national_governing_body_add)
+        @referee.national_governing_bodies << ngb_records if ngb_records.present?
+      end
+
       def find_referee
-        @referee = Referee.find(params[:id])
+        @referee = Referee.find(params.delete(:id))
       end
 
       def permitted_params
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_unprocessable_entity_response(exception)
+    render json: exception.record.errors, status: :unprocessable_entity
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def render_500(exception)
+    render json: { error: exception.message }, status: :internal_server_error
+  end
 end
