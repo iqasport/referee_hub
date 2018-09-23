afc144b9ac4b8c22616d7914a13628ee036cd654
Jannis Grimm <jannis@gje.ch>
2018-09-23 19:26:13 +0200

    Add referee profile edit form

--- app/serializers/certification_serializer.rb
+++ app/serializers/certification_serializer.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: certifications
+#
+#  id           :bigint(8)        not null, primary key
+#  display_name :string           default(""), not null
+#  level        :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_certifications_on_level  (level) UNIQUE
+#
+
 class CertificationSerializer
   include FastJsonapi::ObjectSerializer
 
