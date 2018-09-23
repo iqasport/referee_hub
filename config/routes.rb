978736794a080e15915fb264596a1077dca33f0f
Alexis Moody <amoodyldes@gmail.com>
2018-09-23 13:16:52 -0400

    Readjusts routes so the proper forms are shown

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,22 @@ Rails.application.routes.draw do
 
   post 'webhook', to: 'classmarker#webhook'
 
-  devise_for :referees, controllers: {
-    sessions: 'referees/sessions',
-    passwords: 'referees/passwords',
-    registrations: 'referees/registrations'
-  }
+  devise_scope :referee do
+    get '/sign_up' => 'devise/registrations#new', as: :referee_registration
+    get '/sign_in' => 'devise/sessions#new', as: :new_referee_session
+    post '/sign_in' => 'devise/sessions#create', as: :referee_session
+    get '/sign_out' => 'devise/sessions#destroy', as: :destroy_referee_session
+    get '/password' => 'devise/passwords#new', as: :new_referee_password
+    get '/password/edit' => 'devise/passwords#edit', as: :edit_referee_password
+    patch '/password' => 'devise/passwords#update', as: :update_referee_password
+    post '/password' => 'devise/passwords#create', as: :create_referee_password
+    get 'register/cancel' => 'devise/registrations#cancel', as: :cancel_referee_registration
+    patch '/register' => 'devise/registrations#update', as: :update_referee_registration
+    delete '/register' => 'devise/registrations#destroy', as: :destroy_referee_registration
+    post '/register' => 'devise/registrations#create', as: :create_referee_registration
+  end
+
+  devise_for :referees, skip: :all
 
   root to: 'home#index'
 end
