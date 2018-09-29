7969b037a70c380d715e97ba3044d06dc59177b4
Alexis Moody <amoodyldes@gmail.com>
2018-09-29 12:23:48 -0400

    Adjusts styling and improves error message for new referee registration

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'webpacker'
 
 gem 'fast_jsonapi'
 
+gem 'semantic-ui-sass'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,33 @@
+module DeviseHelper
+  def devise_error_messages!
+    return '' unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    sentence = 'Uh Oh! Looks like there were some issues creating your account'
+
+    html = <<-HTML
+    <div id="error_explanation" class='ui error message'>
+      <h2>#{sentence}</h2>
+      <ul class='list'>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+  def email_error?
+    !resource.errors[:email].empty?
+  end
+
+  def password_error?
+    !resource.errors[:password].empty?
+  end
+
+  def password_confirmation_error?
+    !resource.errors[:password_confirmation].empty?
+  end
+end
