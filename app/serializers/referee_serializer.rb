04b5e6b63c0237e7388d82c14f842c50d29c7058
Alexis Moody <amoodyldes@gmail.com>
2018-09-30 15:52:01 -0400

    Adjusts profile structure for better organization, adds onboarding attribute

--- app/serializers/referee_serializer.rb
+++ app/serializers/referee_serializer.rb
@@ -2,25 +2,26 @@
 #
 # Table name: referees
 #
-#  id                     :bigint(8)        not null, primary key
-#  bio                    :text
-#  current_sign_in_at     :datetime
-#  current_sign_in_ip     :inet
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
-#  first_name             :string
-#  last_name              :string
-#  last_sign_in_at        :datetime
-#  last_sign_in_ip        :inet
-#  pronouns               :string
-#  remember_created_at    :datetime
-#  reset_password_sent_at :datetime
-#  reset_password_token   :string
-#  show_pronouns          :boolean          default(FALSE)
-#  sign_in_count          :integer          default(0), not null
-#  submitted_payment_at   :datetime
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
+#  id                           :bigint(8)        not null, primary key
+#  bio                          :text
+#  current_sign_in_at           :datetime
+#  current_sign_in_ip           :inet
+#  email                        :string           default(""), not null
+#  encrypted_password           :string           default(""), not null
+#  first_name                   :string
+#  getting_started_dismissed_at :datetime
+#  last_name                    :string
+#  last_sign_in_at              :datetime
+#  last_sign_in_ip              :inet
+#  pronouns                     :string
+#  remember_created_at          :datetime
+#  reset_password_sent_at       :datetime
+#  reset_password_token         :string
+#  show_pronouns                :boolean          default(FALSE)
+#  sign_in_count                :integer          default(0), not null
+#  submitted_payment_at         :datetime
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
 #
 # Indexes
 #
