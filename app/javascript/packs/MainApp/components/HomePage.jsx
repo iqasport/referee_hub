7c7e827b8f9d43f02a42cc7943f61886ab81521a
Alexis Moody <amoodyldes@gmail.com>
2018-10-01 15:23:17 -0400

    PR tweaks

--- app/javascript/packs/MainApp/components/HomePage.jsx
+++ app/javascript/packs/MainApp/components/HomePage.jsx
@@ -27,7 +27,7 @@ class HomePage extends Component {
           The home for all international quidditch referees. Here you will find every referee qualified by the IQA based on their certifications, their National Governing Body affiliation, and their name.
         </p>
         <p>
-          As a referee you're able to register and you’ll be able to immediately take written tests to become certified as snitch referee and/or assistant referee. If you passed both tests, you're free to pay for the written head referee test.Soon you'll also be able to find resources to cultivate your skills as a quidditch referee.
+          As a referee you're able to register and you’ll be able to immediately take written tests to become certified as snitch referee and/or assistant referee. If you passed both tests, you're free to pay for the written head referee test. Soon you'll also be able to find resources to cultivate your skills as a quidditch referee.
         </p>
         <Divider />
         <h3>Ready to get started?</h3>
