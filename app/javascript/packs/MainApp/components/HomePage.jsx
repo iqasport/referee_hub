28c4de0cb7b2fbe32f1a6e4ebc1f0a9e95ed96f7
Alexis Moody <amoodyldes@gmail.com>
2018-10-01 04:14:29 -0400

    Fleshes out welcome page and adds some images

--- app/javascript/packs/MainApp/components/HomePage.jsx
+++ app/javascript/packs/MainApp/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Label, Segment, Divider } from 'semantic-ui-react'
 
 class HomePage extends Component {
   static propTypes = {
@@ -8,17 +9,31 @@ class HomePage extends Component {
     }).isRequired
   }
 
-  handleClick = () => {
+  handleRefereeListClick = () => {
     const { history } = this.props
     history.push('/referees')
   }
 
+  handleSignupClick = () => {
+    /* eslint-disable */
+    window.location.assign(`${window.location.origin}/sign_up`)
+  }
+
   render() {
     return (
-      <div className="ui segment">
-        <h1>Welcome to Refs-R-Us</h1>
-        <button type="button" onClick={this.handleClick}>Go to Ref list</button>
-      </div>
+      <Segment textAlign='center'>
+        <h1>Welcome to the Referee Hub</h1>
+        <p>
+          The home for all International Quidditch Referees. As a referee you'll be able to register with the IQA, pay for, and take written tests to become certified. Soon you'll also be able to find resources to cultivate your skills as a quidditch referee.
+        </p>
+        <p>
+          If you're not a referee you do not have to register, but you can search for and view referees based on ther certifications, what National Governing Body they are affiliated with, and their name.
+        </p>
+        <Divider />
+        <h3>Ready to get started?</h3>
+        <Label circular as="a" size="big" onClick={this.handleRefereeListClick} content="View all Referees" />
+        <Label circular as="a" size="big" onClick={this.handleSignupClick} content="Register as a Referee" />
+      </Segment>
     )
   }
 }
