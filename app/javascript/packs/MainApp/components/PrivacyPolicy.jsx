3afa12b4ac57a8d8ce44603ccebabb7cec9a800b
Jannis Grimm <jannis@gje.ch>
2018-10-01 23:51:29 +0200

    Add privacy policy

---
+++ app/javascript/packs/MainApp/components/PrivacyPolicy.jsx
@@ -0,0 +1,198 @@
+import React from 'react'
+import { Header, Segment } from 'semantic-ui-react'
+
+const PrivacyPolicy = () => (
+  <Segment>
+    <Header as="h1">Privacy Policy</Header>
+    <p>
+      We are convinced that the protection of your data protection is important. The use of the
+      International Quidditch Association’s Referee Hub is possible without any indication of
+      personal data; however, if you like to register as a referee, processing of personal data
+      is necessary. If the processing of personal data is necessary and there is no statutory
+      basis for such processing, we generally obtain your consent. Thus we need to provide this
+      privacy policy for inhabitants of the European Union to ask you for your consent. If you
+      live in the European Union, this privacy policy is applicable to you and informs you about
+      personal data we collect and process and about your rights concerning your data.
+    </p>
+    <p>
+      The processing of personal data, such as the name, e-mail address, or gender of a data
+      subject is in line with the General Data Protection Regulation (GDPR). By means of this
+      data protection declaration, we would like to inform the general public of the nature,
+      scope, and purpose of the personal data we collect, use and process. Furthermore, data
+      subjects are informed, by means of this data protection declaration, of the rights to
+      which they are entitled.
+    </p>
+    <p>
+      As the controller, the International Quidditch Association has implemented numerous
+      technical and organizational measures to ensure the most complete protection of personal
+      data processed through this website. However, Internet-based data transmissions may in
+      principle have security gaps, so absolute protection may not be guaranteed.
+    </p>
+    <Header as="h2">
+      Controller
+    </Header>
+    <p>
+      Controller for the purposes of the General Data Protection Regulation (GDPR), other data
+      protection laws applicable in Member states of the European Union and other provisions
+      related to data protection is:
+    </p>
+    <p>
+      International Quidditch Association
+    </p>
+    <p>
+      Email: info@iqasport.org
+    </p>
+    <p>
+      Website: www.iqasport.com
+    </p>
+    <Header as="h2">
+      Cookies
+    </Header>
+    <p>
+      The Referee Hub uses cookies. Cookies are text files that are stored in a computer system
+      via an Internet browser. Many Internet sites and servers use cookies. Many cookies contain
+      a so-called cookie ID. A cookie ID is a unique identifier of the cookie. It consists of a
+      character string through which Internet pages and servers can be assigned to the specific
+      Internet browser in which the cookie was stored. This allows visited Internet sites and
+      servers to differentiate the individual browser of the dats subject from other Internet
+      browsers that contain other cookies. A specific Internet browser can be recognized and
+      identified using the unique cookie ID.
+    </p>
+    <p>
+      Through the use of cookies, we can provide you the possibility to log in and recognize you
+      as the logged in referee. You may, at any time, prevent the setting of cookies through our
+      website by means of a corresponding setting of the Internet browser used, and may thus
+      permanently deny the setting of cookies. Furthermore, already set cookies may be deleted
+      at any time via an Internet browser or other software programs. This is possible in all
+      popular Internet browsers. If you deactivate the setting of cookies in the Internet
+      browser used, the login function is unusable.
+    </p>
+    <Header as="h2">
+      Collection of general data and information
+    </Header>
+    <p>
+      The Referee Hub collects a series of general data and information when a data subject or
+      automated system calls up the website. This general data and information are stored in the
+      server log files. Collected may be (1) the browser types and versions used, (2) the
+      operating system used by the accessing system, (3) the website from which an accessing
+      system reaches our website (so-called referrers), (4) the sub-websites, (5) the date and
+      time of access to the Internet site, (6) an Internet protocol address (IP address), (7)
+      the Internet service provider of the accessing system, and (8) any other similar data and
+      information that may be used in the event of attacks on our information technology
+      systems.
+    </p>
+    <p>
+      When using these general data and information, the International Quidditch Association
+      does not draw any conclusions about you. Rather, this information is needed to (1) deliver
+      the content of our website correctly, (2) optimize the content of our website, (3) ensure
+      the long-term viability of our information technology systems and website technology, and
+      (4) provide law enforcement authorities with the information necessary for criminal
+      prosecution in case of a cyber-attack. Therefore, the International Quidditch Association
+      analyzes anonymously collected data and information statistically, with the aim of
+      increasing the data protection and data security of our enterprise, and to ensure an
+      optimal level of protection for the personal data we process. The anonymous data of the
+      server log files are stored separately from all personal data provided by a data subject.
+    </p>
+    <Header as="h2">
+      Registration
+    </Header>
+    <p>
+      You have the possibility to register on the website of the controller with the indication
+      of personal data. For this reason you may enter your email address. The entered email
+      address is publicly visible on your referee profile. You are free to ask us to change your
+      email address or to delete your referee profile including your email address.
+    </p>
+    <p>
+      We will provide information upon request to you as to what personal data are stored about
+      you. In addition, we will correct or erase personal data at your request or indication,
+      insofar as there are no statutory storage obligations.
+    </p>
+    <p>
+      After the registration with your email address, you are free to add additional personal
+      data to your referee profile, which are not required to use Referee Hub (but may be
+      required to prove your identity to tournament organizers). These include your name, your
+      pronouns, your National Governing Body affiliations and additional information about
+      yourself. Every information entered is visible to anyone on your public referee profile
+      and you can change and remove it at any time. You also have the choice to not show your
+      pronouns on your public referee profile.
+    </p>
+    <Header as="h2">
+      Your Rights
+    </Header>
+    <p>
+      You have the right granted by the European legislator to obtain our confirmation as to
+      whether or not personal data concerning you are being processed, to ask us which data we
+      store about you, to rectify inaccurate personal data, to complete data, to ask us to erase
+      your data, to get your data in machine-readable format for portability, to object to the
+      processing of your personal data and to withdraw your consent. If you wish to avail
+      yourself of this rights, you may, at any time, contact the IQA.
+    </p>
+    <Header as="h2">
+      Payment Method: Data protection provisions about the use of PayPal as a payment
+      processor
+    </Header>
+    <p>
+      On this website, we have integrated components of PayPal. PayPal is an online
+      payment service provider. Payments are processed via so-called PayPal accounts, which
+      represent virtual private or business accounts. PayPal is also able to process virtual
+      payments through credit cards when a user does not have a PayPal account. A PayPal account
+      is managed via an e-mail address, which is why there are no classic account numbers.
+      PayPal makes it possible to trigger online payments to third parties or to receive
+      payments. PayPal also accepts trustee functions and offers buyer protection services.
+    </p>
+    <p>
+      The European operating company of PayPal is PayPal (Europe) S.à.r.l. & Cie. S.C.A., 22-24
+      Boulevard Royal, 2449 Luxembourg, Luxembourg.
+    </p>
+    <p>
+      For you to be able to pay with paypal, we automatically transmit data about you to PayPal.
+      You agree to the transfer of personal data required for payment processing.
+    </p>
+    <p>
+      The transmission of the data is aimed at payment processing and fraud prevention. The
+      controller will transfer personal data to PayPal, in particular, if a legitimate interest
+      in the transmission is given. The personal data exchanged between PayPal and the
+      controller for the processing of the data will be transmitted by PayPal to economic credit
+      agencies. This transmission is intended for identity and creditworthiness checks.
+    </p>
+    <p>
+      PayPal will, if necessary, pass on personal data to affiliates and service providers or
+      subcontractors to the extent that this is necessary to fulfill contractual obligations or
+      for data to be processed in the order.
+    </p>
+    <p>
+      The applicable data protection provisions of PayPal may be retrieved under
+      https://www.paypal.com/us/webapps/mpp/ua/privacy-full.
+    </p>
+    <Header as="h2">
+      Legal basis for the processing
+    </Header>
+    <p>
+      Art. 6(1) lit. a GDPR serves as the legal basis for processing operations for which we
+      obtain consent for a specific processing purpose. If the processing of personal data is
+      necessary for the performance of a contract to which the data subject is party, as is the
+      case, for example, when processing operations are necessary for the supply of goods or to
+      provide any other service, the processing is based on Article 6(1) lit. b GDPR. The same
+      applies to such processing operations which are necessary for carrying out pre-contractual
+      measures, for example in the case of inquiries concerning our products or services. Is our
+      company subject to a legal obligation by which processing of personal data is required,
+      such as for the fulfillment of tax obligations, the processing is based on Art. 6(1) lit.
+      c GDPR. In rare cases, the processing of personal data may be necessary to protect the
+      vital interests of the data subject or of another natural person. This would be the case,
+      for example, if a visitor were injured in our company and his name, age, health insurance
+      data or other vital information would have to be passed on to a doctor, hospital or other
+      third party. Then the processing would be based on Art. 6(1) lit. d GDPR. Finally,
+      processing operations could be based on Article 6(1) lit. f GDPR. This legal basis is used
+      for processing operations which are not covered by any of the abovementioned legal
+      grounds, if processing is necessary for the purposes of the legitimate interests pursued
+      by our company or by a third party, except where such interests are overridden by the
+      interests or fundamental rights and freedoms of the data subject which require protection
+      of personal data. Such processing operations are particularly permissible because they
+      have been specifically mentioned by the European legislator. He considered that a
+      legitimate interest could be assumed if the data subject is a client of the controller
+      (Recital 47 Sentence 2 GDPR).
+    </p>
+  </Segment>
+)
+
+export default PrivacyPolicy
