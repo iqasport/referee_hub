19567017d8771916ee56a7a11fbce3887c5d02da
Alexis Moody <amoodyldes@gmail.com>
2018-10-06 11:54:38 -0400

    Adjusts some configs from testing

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
   config.mailer_sender = 'noreply@iqareferees.org'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
