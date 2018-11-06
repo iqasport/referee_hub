7e3b22b6fab8e3b03028cbb1db67dd459dc2c1f7
Alexis Moody <amoodyldes@gmail.com>
2018-11-05 19:37:59 -0500

    Adds controller and serializer to update needs_renewal_at

---
+++ app/controllers/api/v1/referee_certifications_controller.rb
@@ -0,0 +1,39 @@
+module Api
+  module V1
+    class RefereeCertificationsController < ::ApplicationController
+      before_action :authenticate_referee!
+      before_action :find_referee_certification, only: :update
+      skip_before_action :verify_authenticity_token
+
+      layout false
+
+      def index
+        @referee_certifications = current_referee.referee_certifications
+
+        json_string = RefereeCertificationSerializer.new(@referee_certifications).serialized_json
+
+        render json: json_string, status: :ok
+      end
+
+      def update
+        if @referee_certification.update!(permitted_params)
+          json_string = RefereeCertificationSerializer.new(@referee_certification)
+
+          render json: json_string, status: :ok
+        else
+          render json: { error: @referee_certification.errors.message }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def find_referee_certification
+        @referee_certification = current_referee.referee_certifications.find_by(id: params[:id])
+      end
+
+      def permitted_params
+        params.permit(:needs_renewal_at)
+      end
+    end
+  end
+end
