c1d285dc465f1a533d1b4dbcad7b86964aedaf2c
Alexis Moody <amoodyldes@gmail.com>
2018-12-24 17:12:52 -0500

    Adds admin routing components to ref diag and skeleton ref diag

---
+++ app/controllers/api/v1/diagnostic_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class DiagnosticController < ApplicationController
+      before_action :authenticate_referee!
+      skip_before_action :verify_authenticity_token
+
+      layout false
+
+      def search
+        if referee = find_referee
+          json_string = RefereeSerializer.new(
+            referee,
+            include: %i[referee_certifications certifications national_governing_bodies test_attempts test_results],
+            params: { current_user: current_referee, include_tests: true }
+          ).serialized_json
+
+          render json: json_string, status: :ok
+        else
+          error_message = 'Referee not found, please check the email provided'
+          render json: { error: error_message }, status: :not_found
+        end
+      end
+
+      private
+
+      def permitted_params
+        params.permit(:referee_search)
+      end
+
+      def find_referee
+        search_query = permitted_params.delete(:referee_search)
+
+        Referee.find_by email: search_query
+      end
+    end
+  end
+end
---
+++ app/javascript/packs/MainApp/components/RefereeDiagnostic.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RefereeDiagnostic = () => {
+  return <div>Hello Ref Diag</div>
+}
+
+export default RefereeDiagnostic
