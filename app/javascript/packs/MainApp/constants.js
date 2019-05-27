968abfaabad4383f78543bf8bf51db4db2a41840
Alexis Moody <amoodyldes@gmail.com>
2019-05-27 16:11:48 -0400

    Adds create test button and cleans up test form

--- app/javascript/packs/MainApp/constants.js
+++ app/javascript/packs/MainApp/constants.js
@@ -1,3 +1,6 @@
+import { Input, TextArea, Dropdown } from 'semantic-ui-react'
+import { range } from 'lodash'
+
 export const currencyConfig = [
   {
     total: '23',
@@ -26,4 +29,86 @@ export const CERTIFICATION_LEVELS = {
   assistant: 'assistant',
   head: 'head',
   field: 'field'
+}
+
+const levelDropdown = [
+  { text: 'Snitch', value: 'snitch' },
+  { text: 'Assistant', value: 'assistant' },
+  { text: 'Head', value: 'value' }
+]
+const MAX_TIME = 120
+const MIN_TIME = 10
+const minuteArray = range(MIN_TIME, MAX_TIME)
+const timeLimitDropdown = minuteArray.map(minute => ({
+  text: minute,
+  value: minute
+}))
+
+export const TEST_FORM_CONFIG = (values) => {
+  const {
+    name, description, level, language, minimumPassPercentage, timeLimit, positiveFeedback, negativeFeedback
+  } = values
+
+  const descPlaceholder = !description ? "What should referee's know about this test before taking it?" : null
+  const negFeedbackPlaceholder = !negativeFeedback ? 'Provide feedback after a failed test' : null
+  const posFeedbackPlaceholder = !positiveFeedback ? 'Provide feedback after a passed test' : null
+
+  return [
+    {
+      control: Input,
+      label: 'Name',
+      name: 'Name',
+      value: name,
+    },
+    {
+      control: TextArea,
+      label: 'Description',
+      name: 'Description',
+      value: description,
+      placeholder: descPlaceholder,
+    },
+    {
+      control: Input,
+      label: 'Language',
+      name: 'Language',
+      value: language,
+    },
+    {
+      control: Dropdown,
+      label: 'Certification Level',
+      name: 'Level',
+      options: levelDropdown,
+      value: level || undefined,
+      defaultValue: !level ? 'snitch' : undefined,
+    },
+    {
+      control: Input,
+      label: 'Minimum Pass Percentage',
+      name: 'MinimumPassPercentage',
+      value: minimumPassPercentage,
+    },
+    {
+      control: Dropdown,
+      label: 'Time Limit',
+      name: 'TimeLimit',
+      options: timeLimitDropdown,
+      value: timeLimit || undefined,
+      defaultValue: !timeLimit ? 18 : undefined,
+      scrolling: 'true'
+    },
+    {
+      control: TextArea,
+      label: 'Positive Feedback',
+      name: 'PositiveFeedback',
+      value: positiveFeedback,
+      placeholder: posFeedbackPlaceholder,
+    },
+    {
+      control: TextArea,
+      label: 'Negative Feedback',
+      name: 'NegativeFeedback',
+      value: negativeFeedback,
+      placeholder: negFeedbackPlaceholder,
+    },
+  ]
 }
