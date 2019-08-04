a99fab51aaf2359426d0dfd02149504ca9d76555
Alexis Moody <amoodyldes@gmail.com>
2019-08-04 16:49:10 -0400

    Adjusts grading processing to use async queue

---
+++ spec/jobs/grade_job_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe GradeJob, type: :job do
+  let(:test_to_grade) { create :test }
+  let(:ref) { create :referee }
+  let(:test_timestamps) { { started_at: Time.now.utc.to_s, finished_at: Time.now.utc.to_s } }
+  let(:referee_answers) { [{ question_id: 1, answer_id: 2 }] }
+
+  subject { described_class.perform_later(test_to_grade, ref, test_timestamps, referee_answers) }
+
+  it 'should enqueue the job with the correct params' do
+    ActiveJob::Base.queue_adapter = :test
+
+    expect { subject }.to have_enqueued_job
+  end
+end
