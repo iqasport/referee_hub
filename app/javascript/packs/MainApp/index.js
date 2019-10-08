9e151f964762f127ada8e0eda2f8eceea5a5836c
Alexis Moody <amoodyldes@gmail.com>
2019-10-08 10:06:03 -0400

    Adds bugsnag error boundary to app render

--- app/javascript/packs/MainApp/index.js
+++ app/javascript/packs/MainApp/index.js
@@ -2,7 +2,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
+import bugsnag from '@bugsnag/js'
+import bugsnagReact from '@bugsnag/plugin-react'
+
+var bugsnagClient = bugsnag({
+  apiKey: process.env.BUGSNAG_API_KEY,
+  autoCaptureSessions: false,
+});
+bugsnagClient.use(bugsnagReact, React);
+var ErrorBoundary = bugsnagClient.getPlugin('react');
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<Routes />, document.getElementById('main-app'));
+  ReactDOM.render(
+    <ErrorBoundary>
+      <Routes />
+    </ErrorBoundary>,
+    document.getElementById('main-app')
+  );
 });
