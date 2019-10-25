9c64f1651373a3d34e4f01cc223a4f1cb0d06b11
Alexis Moody <amoodyldes@gmail.com>
2019-10-25 08:14:09 -0400

    Adjusts sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,16 @@
 require 'sidekiq'
 require 'sidekiq-status'
 
-redis_url = ENV['REDIS_URL'] || ENV['REDISTOGO_URL']
+redis_url = ENV['REDIS_PROVIDER'] || ENV['REDISTOGO_URL']
 
 Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
+
   Sidekiq::Status.configure_client_middleware config
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :url => redis_url }
+  config.redis = { url: redis_url }
 
   Sidekiq::Status.configure_client_middleware config
   Sidekiq::Status.configure_server_middleware config
