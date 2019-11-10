f7a45f7def346a0419a16f336475566432ec1d94
Alexis Moody <amoodyldes@gmail.com>
2019-11-10 10:43:05 -0500

    Adds source map to production webpack config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,23 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+const { BugsnagBuildReporterPlugin, BugsnagSourceMapUploaderPlugin } = require('webpack-bugsnag-plugins')
 
 const environment = require('./environment')
+
+const apiKey = process.env.RAILS_ENV !== 'production' ? 'iamAFak3apiKey' : process.env.BUGSNAG_API_KEY
+const buildReporter = new BugsnagBuildReporterPlugin({
+  apiKey,
+  sourceControl: {
+    provider: 'github',
+    repository: 'https://github.com/iqasport/referee_hub'
+  },
+  autoAssignRelease: true
+})
+const sourceMap = new BugsnagSourceMapUploaderPlugin({
+  apiKey,
+  publicPath: '/packs/'
+})
+
+environment.plugins.prepend('BugsnagBuildReporterPlugin', buildReporter)
+environment.plugins.prepend('BugsnagSourceMapUploaderPlugin', sourceMap)
 
 module.exports = environment.toWebpackConfig()
