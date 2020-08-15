70c608fce25e3caf011e35b50d038d92b29a064d
Alexis Moody <amoody@morningconsult.com>
2020-08-15 16:18:25 -0400

    Adjusts stats bar styling and ngb profile view on mobile

--- app/javascript/MainApp/components/StatBarChart/StatBarChart.tsx
+++ app/javascript/MainApp/components/StatBarChart/StatBarChart.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Bar, BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import { Bar, BarChart, CartesianGrid, LabelList, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 
 type ChartData = {
   type: string;
@@ -22,7 +22,11 @@ interface StatBarChartProps {
 const StatBarChart = (props: StatBarChartProps) => {
   const { chartData, barConfig, maxData } = props
 
-  const renderBar = (bar: BarConfig) => <Bar key={bar.dataKey} {...bar} />
+  const renderBar = (bar: BarConfig) => (
+    <Bar key={bar.dataKey} {...bar} minPointSize={5}>
+      <LabelList dataKey="type" position="center" angle={270} />
+    </Bar>
+  )
 
   return (
     <ResponsiveContainer height="100%" width="80%">
@@ -30,12 +34,12 @@ const StatBarChart = (props: StatBarChartProps) => {
         width={250}
         height={250}
         data={chartData}
-        barSize={40}
+        barSize={60}
         barCategoryGap="5%"
         margin={{ top: 20, bottom: 5, right: 2, left: -20 }}
       >
         <CartesianGrid vertical={false} />
-        <XAxis dataKey="type" />
+        <XAxis dataKey="type" tick={false} />
         <YAxis type="number" domain={[0, maxData]} />
         {barConfig.map(renderBar)}
       </BarChart>
