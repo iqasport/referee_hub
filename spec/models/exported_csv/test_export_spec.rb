623eacd8fc9be443a5af0a7d0847e61172ae2e71
Alexis Moody <amoody@morningconsult.com>
2020-11-13 10:34:17 -0500

    Adds more tests for test_export

--- spec/models/exported_csv/test_export_spec.rb
+++ spec/models/exported_csv/test_export_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ExportedCsv::TestExport, type: :model do
-  include ActiveJob::TestHelper
-
   let(:user) { create :user }
   let(:csv) { user.exported_csvs.last }
   let!(:test) { create :test }
@@ -16,8 +14,6 @@ RSpec.describe ExportedCsv::TestExport, type: :model do
       create_list(:answer, 4, question_id: q.id)
       q.answers.each_with_index {|a, idx| a.update(correct: true) if idx == correct_index }
     end
-
-    Fog.mock!
   end
 
   subject { described_class.new(user: user, export_options: export_options) }
@@ -32,6 +28,10 @@ RSpec.describe ExportedCsv::TestExport, type: :model do
     )
   end
 
+  it 'generates csv data' do
+    expect(subject.generate_csv_data).to_not be_nil
+  end
+
   context 'without any correct answers' do
     let(:correct_index) { 100 }
 
@@ -43,6 +43,14 @@ RSpec.describe ExportedCsv::TestExport, type: :model do
           '',
         ].push(question.answers.map {|a| a.description }).flatten
       )
+    end
+  end
+
+  context 'without a test_id' do
+    let(:export_options) { {}.to_json }
+
+    it 'raises an error' do
+      expect { subject.generate_csv_data }.to raise_error(StandardError)
     end
   end
 end
