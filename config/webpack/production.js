46eb659437cdb37f0402011b8e7786fc7162930e
Alexis Moody <amoody@morningconsult.com>
2021-02-06 09:39:17 -0500

    Move dependency

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,7 +1,10 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
-const environment = require('./environment')
+const environment = require("./environment");
 
-environment.loaders.get('babel').exclude = [/\.test\.js$/, environment.loaders.get('babel').exclude];
+environment.loaders.get("babel").exclude = [
+  /\.test\.(ts|tsx)$/,
+  environment.loaders.get("babel").exclude,
+];
 
-module.exports = environment.toWebpackConfig()
+module.exports = environment.toWebpackConfig();
