4ff577726c8fa546386c294e8e6097012d212140
Marian Dziubiak <marian.dziubiak@gmail.com>
2022-11-01 20:13:07 +0000

    Fix error when importing test CSV with custom headers

--- spec/services/test_csv_import_spec.rb
+++ spec/services/test_csv_import_spec.rb
@@ -53,4 +53,46 @@ describe Services::TestCsvImport do
       expect { subject }.to raise_error { Services::TestCsvImport::TestMissingError }
     end
   end
+
+  context 'with custom headers' do
+    let(:headers) do
+      'q description,feedback,points available,correct answer,answer 1,answer 2,answer 3,answer 4'
+    end
+    let(:row1) { 'to be or not to be?,do better,1,answer 1,be,maybe,not be,never' }
+    let(:row2) { 'should you be the best?,also do better,1,answer 2,yes,sometimes,no,never' }
+    let(:row3) { "are you already the best?,why can't you do better,1,answer 3,possibly,maybe,kind of,never" }
+    let(:rows) { [headers, row1, row2, row3] }
+    let!(:csv) do
+      CSV.open(file_path, 'w') do |csv|
+        rows.each { |row| csv << row.split(',') }
+      end
+    end
+    let(:mapped_headers) do
+      {
+        'description': 'q description',
+        'feedback': 'feedback',
+        'points_available': 'points available',
+        'correct_answer': 'correct answer',
+        'answer_1': 'answer 1',
+        'answer_2': 'answer 2',
+        'answer_3': 'answer 3',
+        'answer_4': 'answer 4'
+      }
+    end
+
+    subject { described_class.new(file_path, test_to_import, mapped_headers.to_json).perform }
+
+    it 'creates 3 questions with answers' do
+      expect { subject }.to change { test_to_import.questions.count }.by 3
+    end
+  
+    it 'creates the answers for the provided questions' do
+      subject
+  
+      expect(test_to_import.questions.first.answers.count).to eq 4
+      expect(test_to_import.questions.first.answers.pluck(:description)).to eq ['be', 'maybe', 'not be', 'never']
+      expect(test_to_import.questions.first.answers.first.correct).to eq true
+      expect(test_to_import.questions.first.answers.last.correct).to eq false
+    end
+  end
 end
