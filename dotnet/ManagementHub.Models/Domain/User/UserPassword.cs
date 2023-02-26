ef7ce3e6531e8ec5d06dde7a23a6b898920bd050
Marian Dziubiak <marian.dziubiak@gmail.com>
2023-02-26 13:28:00 +0000

    Add DbUserAuthenticationContext

--- dotnet/ManagementHub.Models/Domain/User/UserPassword.cs
+++ dotnet/ManagementHub.Models/Domain/User/UserPassword.cs
@@ -1,19 +1,8 @@
 namespace ManagementHub.Models.Domain.User;
 
-public class UserPassword
+/// <summary>
+/// A BCrypt hash of the password (with the algorithm version and salt).
+/// </summary>
+public record UserPassword(string PasswordHash)
 {
-    public UserPassword(string passwordHash)
-    {
-        this.PasswordHash = passwordHash;
-    }
-    
-    /// <summary>
-    /// A BCrypt hash of the password (with the algorithm version and salt).
-    /// </summary>
-    public string PasswordHash { get; set; }
-
-    /// <summary>
-    /// Details of the password reset operation.
-    /// </summary>
-    public PasswordReset? PasswordReset { get; set;} 
 }
---
+++ dotnet/ManagementHub.Storage/Contexts/DbUserAuthenticationContext.cs
@@ -0,0 +1,52 @@
+﻿using ManagementHub.Models.Abstraction.Contexts;
+using ManagementHub.Models.Data;
+using ManagementHub.Models.Domain.General;
+using ManagementHub.Models.Domain.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ManagementHub.Storage.Contexts;
+
+internal record DbUserAuthenticationContext(UserIdentifier UserId, UserPassword UserPassword) : IUserAuthenticationContext
+{
+}
+
+internal class DbUserAuthenticationContextFactory
+{
+	private readonly IQueryable<User> users;
+	private readonly ILogger<DbUserContextFactory> logger;
+
+	public DbUserAuthenticationContextFactory(
+		IQueryable<User> users,
+		ILogger<DbUserContextFactory> logger)
+	{
+		this.users = users;
+		this.logger = logger;
+	}
+
+	public async Task<DbUserAuthenticationContext?> TryLoadAsync(Email userEmail, CancellationToken cancellationToken)
+	{
+		this.logger.LogInformation(0, "Loading user authentication context.");
+		var user = await this.users.Where(user => user.Email == userEmail.Value)
+			.Select(user => new User
+			{
+				Id = user.Id,
+				EncryptedPassword = user.EncryptedPassword,
+			})
+			.SingleOrDefaultAsync(cancellationToken);
+
+		if (user is null)
+		{
+			this.logger.LogInformation(0, "User for the specified email address was not found.");
+			return null;
+		}
+
+		var userId = new UserIdentifier(user.Id);
+		var userPassword = new UserPassword(user.EncryptedPassword);
+
+		this.logger.LogInformation(0, "Loaded authentication context for user ({userId}).", userId);
+
+		return new DbUserAuthenticationContext(userId, userPassword);
+	}
+}
+
