629562802dbc0354004f77841409c30e86dd7cb7
Marian Dziubiak <marian.dziubiak@gmail.com>
2023-02-26 12:40:51 +0000

    Add equality check to NgbConstraint

--- dotnet/ManagementHub.Models/Domain/Ngb/NgbConstraint.cs
+++ dotnet/ManagementHub.Models/Domain/Ngb/NgbConstraint.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace ManagementHub.Models.Domain.Ngb;
@@ -7,15 +8,15 @@ namespace ManagementHub.Models.Domain.Ngb;
 /// </summary>
 public class NgbConstraint
 {
-    private bool anyNgb;
-    private List<NgbIdentifier>? nationalGoverningBodies;
+    private readonly bool anyNgb;
+    private readonly HashSet<NgbIdentifier>? nationalGoverningBodies;
     
     private NgbConstraint() => anyNgb = true;
 
     public NgbConstraint(NgbIdentifier nationalGoverningBody)
-        => this.nationalGoverningBodies = new List<NgbIdentifier>{nationalGoverningBody};
+        => this.nationalGoverningBodies = new HashSet<NgbIdentifier>{nationalGoverningBody};
     public NgbConstraint(IEnumerable<NgbIdentifier> nationalGoverningBodies)
-        => this.nationalGoverningBodies = new List<NgbIdentifier>(nationalGoverningBodies);
+        => this.nationalGoverningBodies = new HashSet<NgbIdentifier>(nationalGoverningBodies);
 
     public static NgbConstraint Any = new NgbConstraint();
 
@@ -28,4 +29,26 @@ public class NgbConstraint
 
         return this.nationalGoverningBodies!.Contains(ngbId);
     }
+
+	public override bool Equals(object? obj)
+	{
+		return obj is NgbConstraint other &&
+			this.anyNgb == other.anyNgb &&
+			(
+				(
+					this.nationalGoverningBodies != null && other.nationalGoverningBodies != null &&
+					this.nationalGoverningBodies.SetEquals(other.nationalGoverningBodies)
+				) ||
+				this.nationalGoverningBodies == other.nationalGoverningBodies // when one of them is null or they're not set equal - this checks that they're both null
+			);
+	}
+
+	public override int GetHashCode()
+	{
+		int hashCode = anyNgb.GetHashCode();
+		if (this.nationalGoverningBodies != null)
+			foreach(var ngb in this.nationalGoverningBodies)
+				hashCode = HashCode.Combine(hashCode, ngb.GetHashCode());
+		return hashCode;
+	}
 }
