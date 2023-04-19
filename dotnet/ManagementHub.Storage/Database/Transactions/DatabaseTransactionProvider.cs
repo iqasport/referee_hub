ada93d70eb6b3affb3ebe323bdeb510d936d667b
Marian Dziubiak <marian.dziubiak@gmail.com>
2023-04-19 07:50:19 +0100

    Make transactions fall up to broadest scope

--- dotnet/ManagementHub.Storage/Database/Transactions/DatabaseTransactionProvider.cs
+++ dotnet/ManagementHub.Storage/Database/Transactions/DatabaseTransactionProvider.cs
@@ -1,5 +1,8 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
+using System.Threading;
 using System.Threading.Tasks;
+using ManagementHub.Models.Exceptions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 
@@ -15,7 +18,51 @@ internal class DatabaseTransactionProvider : IDatabaseTransactionProvider
 
 	public Task<IDbContextTransaction> BeginAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted)
 	{
+		// if a transaction is already in progress in the outer scope operations will be included in that one,
+		// so here we will return an object that ignores commit, but propagates rollback by throwing an exception.
+		if (this.dbContext.Database.CurrentTransaction != null)
+		{
+			return Task.FromResult<IDbContextTransaction>(new InnerTransaction(this.dbContext.Database.CurrentTransaction));
+		}
+
 		return this.dbContext.Database.BeginTransactionAsync(isolationLevel);
 	}
 
+	private class InnerTransaction : IDbContextTransaction
+	{
+		private readonly IDbContextTransaction outerTransaction;
+
+		public InnerTransaction(IDbContextTransaction outerTransaction)
+		{
+			this.outerTransaction = outerTransaction;
+		}
+
+		public Guid TransactionId => this.outerTransaction.TransactionId;
+
+		// no-op
+		public void Commit() { }
+
+		// no-op
+		public Task CommitAsync(CancellationToken cancellationToken = default) => Task.CompletedTask;
+
+		// no-op
+		public void Dispose() { }
+
+		// no-op
+		public ValueTask DisposeAsync() => ValueTask.CompletedTask;
+
+		public void Rollback()
+		{
+			this.outerTransaction.Rollback();
+			throw this.GetException();
+		}
+
+		public async Task RollbackAsync(CancellationToken cancellationToken = default)
+		{
+			await this.outerTransaction.RollbackAsync(cancellationToken);
+			throw this.GetException();
+		}
+
+		private Exception GetException() => new TransactionRollbackException("Rollback was called in an inner transaction which had to disrupt the outer transaction in an uncontrolled way.");
+	}
 }
--- dotnet/ManagementHub.Storage/Database/Transactions/IDatabaseTransactionProvider.cs
+++ dotnet/ManagementHub.Storage/Database/Transactions/IDatabaseTransactionProvider.cs
@@ -6,5 +6,10 @@ namespace ManagementHub.Storage.Database.Transactions;
 
 public interface IDatabaseTransactionProvider
 {
+	/// <summary>
+	/// Starts a new transaction if one is not already running and returns an object for managing the current transaction context.
+	/// </summary>
+	/// <param name="isolationLevel">Isolation level of the transaction which describes how data of other workers will be viewable in the current context.</param>
+	/// <returns>A disposable transaction object.</returns>
 	Task<IDbContextTransaction> BeginAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted);
 }
