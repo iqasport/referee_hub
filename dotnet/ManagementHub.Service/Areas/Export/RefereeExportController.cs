c5d5645216212337eed841e8bcd42dba846ccb4b
Marian Dziubiak <marian.dziubiak@gmail.com>
2023-04-19 08:49:30 +0100

    Create stubs of two more controllers to implement

---
+++ dotnet/ManagementHub.Service/Areas/Export/RefereeExportController.cs
@@ -0,0 +1,45 @@
+﻿using ManagementHub.Models.Domain.Ngb;
+using ManagementHub.Models.Domain.User.Roles;
+using ManagementHub.Models.Exceptions;
+using ManagementHub.Service.Authorization;
+using ManagementHub.Service.Contexts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementHub.Service.Areas.Export;
+
+/// <summary>
+/// Actions related to exporting users with the referee role.
+/// </summary>
+[ApiController]
+[Route("api/v2/referees/export")]
+[Produces("application/json")]
+public class RefereeExportController : ControllerBase
+{
+	private readonly IUserContextAccessor contextAccessor;
+
+	public RefereeExportController(IUserContextAccessor contextAccessor)
+	{
+		this.contextAccessor = contextAccessor;
+	}
+
+	[HttpPost("{ngb}")]
+	[Authorize(AuthotizationPolicies.RefereeViewerPolicy)]
+	public async Task ExportRefereesForNgb([FromQuery]NgbIdentifier ngbIdentifier)
+	{
+		var userContext = await this.contextAccessor.GetCurrentUserContextAsync();
+		var refereeViewerRole = userContext.Roles.OfType<RefereeViewerRole>().FirstOrDefault();
+		if (refereeViewerRole == null)
+		{
+			throw new AccessDeniedException(nameof(RefereeViewerRole));
+		}
+
+		if (!refereeViewerRole.Ngb.AppliesTo(ngbIdentifier))
+		{
+			throw new AccessDeniedException(ngbIdentifier.ToString());
+		}
+
+		// TODO: schedule a job for exporting referee data for the NGB using the IRefereeContextProvider and sent over email to the current user
+		throw new NotImplementedException();
+	}
+}
