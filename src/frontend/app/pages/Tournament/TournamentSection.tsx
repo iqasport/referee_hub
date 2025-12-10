8da5bf3472f6f8954be8a5ae9e4b313ccff1dd25
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-12-10 14:45:07 +0100

    fixed section title

--- src/frontend/app/pages/Tournament/TournamentSection.tsx
+++ src/frontend/app/pages/Tournament/TournamentSection.tsx
@@ -33,7 +33,7 @@ const TournamentSection: React.FC<TournamentSectionProps> = ({ tournaments, visi
     <section>
       {visibility && (
         <h2 className="text-2xl font-bold text-gray-900 mb-4">
-          {visibility === "public" ? "Public" : "Private"} tournaments
+          {visibility === "public" ? "Public" : "Your"} Tournaments
         </h2>
       )}
       <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto max-w-[80%]">
