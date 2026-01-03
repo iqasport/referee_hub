17974db3309f191634525b08ec6933eb7452d700
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2026-01-03 14:09:17 +0100

    using the navigationutils in the tournament section but running into a problem

--- src/frontend/app/pages/Tournaments/components/TournamentsSection.tsx
+++ src/frontend/app/pages/Tournaments/components/TournamentsSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import TournamentCard from "./TournamentCard";
 import { TournamentType } from "../../../store/serviceApi";
+import { useNavigate } from "../../../utils/navigationUtils";
 
 export type Manager = { id: string; name: string };
 
